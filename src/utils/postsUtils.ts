b45fcd288c5932d96b9b63d7cb0ee61fcd0df573
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-04-15 02:17:25 +0530

    Removed postsUtils.

--- src/utils/postsUtils.ts
+++
@@ -1,8 +0,0 @@
-export const getColorFromCategory = (category:string) => {
-    switch(category){
-        case 'Sustainability': return 'lime';
-        case 'Innovation' : return 'cyan';
-        case 'Security' : return 'violet';
-        default : return 'fuchsia'
-    }
-}
